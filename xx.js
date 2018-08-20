f557107ace4fd8fb414f31199a7771219ae89aac
pritampaul1993 <42534774+pritampaul1993@users.noreply.github.com>
2018-08-20 13:38:28 +0530

    Create xx.js

---
+++ xx.js
@@ -0,0 +1,3 @@
+<script src='//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js' type='text/javascript'/>
+<script src='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML' type='text/javascript'/>
+<script async='' src='https://cdn.onesignal.com/sdks/OneSignalSDK.js'/>
